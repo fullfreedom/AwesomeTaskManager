9012888dd073421038b584448cf398d77a1fff13
nordman <fullfreedoms@gmail.com>
2017-06-18 23:53:40 +0500

    Task Manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="wrapper">
+      <h1>Awesome task manager</h1>
+      <div class="content">
+        <app-task-list></app-task-list>
+      </div>
+      <div class="sidebar">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .wrapper {
+      width: 1000px;
+      margin: 0 auto;
+    }
+    .content {
+        width: 680px;
+        float: left;
+    }
+    .sidebar {
+        width: 290px;
+        float: left;
+        padding-left: 30px;
+    }
+  `]
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes }   from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './tasks/task-list.component';
+import { TaskDetailComponent } from './tasks/task-detail.component';
+import { TaskFormComponent } from './tasks/task-form.component';
+import { TaskEditComponent } from './tasks/task-edit.component';
+import { TaskService } from './tasks/task.service';
+
+const appRoutes: Routes = [
+  {
+    path: 'detail/:index',
+    component: TaskDetailComponent,
+  },
+  {
+    path: 'edit/:index',
+    component: TaskEditComponent
+  },
+  {
+    path: 'create',
+    component: TaskFormComponent
+  },
+  {
+    path: '',
+    redirectTo: '/create',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/create'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskDetailComponent,
+    TaskFormComponent,
+    TaskEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+      TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
